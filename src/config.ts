34aadbfe78a3fd1e9afb101933b068f50c78c6b5
Almango <sliveralmango@outlook.com>
2026-02-09 22:44:56 +0800

    'v0.0.3'

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+
+export const SiteConfig = {
+  title: 'NavGo',
+  favicon: '/favicon.ico',
+  desc: '化繁为简，心归纯粹',
+  url: 'https://navgo.almango.cn',
+}
+
+
+export const DEVELOPER_CONFIG = {
+  THEME_VERSION: '0.0.3',
+}
